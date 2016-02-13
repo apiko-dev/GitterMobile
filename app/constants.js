e4b79099dc1f1a3302e88c7c3412fbbff801461a
Terry Sahaidak <s_tasssik_taras@ukr.net>
2016-02-13 03:11:44 +0200

    Add constants file | add gitterDefault theme and colors

---
+++ app/constants.js
@@ -0,0 +1,30 @@
+export const THEMES = {
+  gitterDefault: {
+    name: 'Gitter default theme',
+    colors: {
+      sidebarBackground: '#383435',
+
+      primaryButton: '#46bc99',
+      secondaryButton: '#d6d6d6',
+
+      green: '#1dce73',
+      orange: '#ea9448',
+      yellow: '#f1c40f',
+      blue: '#3498db',
+      red: '#e74c3c',
+      purple: '#935991',
+
+      mention: '#e67e22',
+      unReadBackground: 'rgba(213,245,226,.8)',
+
+      mainFont: '#333',
+      secondaryFont: '#777',
+      whiteFont: 'white',
+      link: '#3498db',
+      blockquoteBorder: '#eee',
+
+      codeInlineBorder: 'rgba(192,201,200,.4)',
+      codeInlineBackground: 'rgba(192,201,200,.2'
+    }
+  }
+}
