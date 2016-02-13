29684d8c12e740a5905dcf0a7cf88ac4765f6315
Terry Sahaidak <s_tasssik_taras@ukr.net>
2016-02-14 00:36:37 +0200

    Add React Native Material Kit by @xinthink

--- app/styles/LoginStyles.js
+++ app/styles/LoginStyles.js
@@ -3,19 +3,7 @@ import {StyleSheet} from 'react-native'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
   }
 })
 
--- app/styles/LoginWelcomeStyles.js
+++ app/styles/LoginWelcomeStyles.js
@@ -1,7 +1,10 @@
 import {StyleSheet} from 'react-native'
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF'
+  }
 })
 
 export default styles
