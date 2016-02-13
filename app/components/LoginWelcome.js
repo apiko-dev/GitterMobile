6ff45743f72bb39a3353f5419bfaa35d12cfd355
Terry Sahaidak <s_tasssik_taras@ukr.net>
2016-02-14 01:59:17 +0200

    Style LoginWelcome

--- app/components/LoginWelcome.js
+++ app/components/LoginWelcome.js
@@ -2,18 +2,45 @@ import React, {
   Component,
   PropTypes,
   Text,
+  Image,
   View
 } from 'react-native'
 import s from '../styles/LoginWelcomeStyles'
+import {MKButton} from 'react-native-material-kit'
+
 
 export default class LoginWelcome extends Component {
   render() {
     return (
-      <View style={s.container}>
-        <Text style={s.welcome}>
-          Login welcome.
+      <Image style={s.container}
+        source={require('../images/gitter-background.jpg')}>
+        <Text style={s.logo}>
+          GitterMobile
         </Text>
-      </View>
+        <Text style={s.hero}>
+          To start using Gitter mobile you should login first.
+          You can login by oauth2 through WebView or just
+          copy/paste authentication token.
+        </Text>
+        <View style={s.buttonGroup}>
+          <MKButton
+            style={s.buttonStyle}
+            onPress={() => {}}>
+            <Text pointerEvents="none"
+                  style={{color: 'white', fontWeight: 'bold'}}>
+              Login by WebView
+            </Text>
+          </MKButton>
+          <MKButton
+            style={s.buttonStyle}
+            onPress={() => {}}>
+            <Text pointerEvents="none"
+                  style={{color: 'white', fontWeight: 'bold'}}>
+              Login by token
+            </Text>
+          </MKButton>
+        </View>
+      </Image>
     )
   }
 }
---
+++ app/styles/common/BackgroundImage.js
@@ -0,0 +1,11 @@
+import {Dimensions} from 'react-native'
+
+const {width, height} = Dimensions.get('window')
+
+const styles = {
+  flex: 1,
+  width,
+  height
+}
+
+export default styles
