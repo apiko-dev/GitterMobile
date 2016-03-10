1259d15c97a4567fbc3ab2e324c926b9a80be188
Terry Sahaidak <s_tasssik_taras@ukr.net>
2016-03-10 18:45:17 +0200

    Add launch screen

---
+++ app/screens/LaunchScreen.js
@@ -0,0 +1,21 @@
+import React, {
+  Component,
+  Text,
+  Image
+} from 'react-native'
+import s from '../styles/LaunchScreenStyles'
+
+
+export default class LaunchScreen extends Component {
+  render() {
+    return (
+      <Image style={s.container}
+        source={require('../images/gitter-background.jpg')}>
+
+        <Text style={s.logo}>
+          Loading...
+        </Text>
+      </Image>
+    )
+  }
+}
