758013ad840cf7cc8d487ff2624d445b772ddd48
Terry Sahaidak <s_tasssik_taras@ukr.net>
2016-03-20 18:28:47 +0200

    Add base module

--- app/modules/navigation.js
+++ app/modules/navigation.js
@@ -1,84 +1,63 @@
-import {nav} from '../screens'
+import {NavigationStateUtils} from 'react-native'
 
 const NAVIGATE_TO = 'navigation/NAVIGATE_TO'
 const NAVIGATE_BACK = 'navigation/NAVIGATE_BACK'
 const NAVIGATE_REPLACE = 'navigation/NAVIGATE_REPLACE'
 const NAVIGATE_RESET = 'navigation/NAVIGATE_RESET'
-const UPDATE_HISTORY = 'navigation/UPDATE_HISTORY'
 
-export function goTo(route) {
-  return dispatch => {
-    dispatch({type: NAVIGATE_TO, route})
-    nav.push(route)
-    dispatch({type: UPDATE_HISTORY})
+export function goTo(state) {
+  return {
+    type: NAVIGATE_TO,
+    state
   }
 }
 
 export function goBack() {
-  return dispatch => {
-    nav.pop()
-    dispatch({type: NAVIGATE_BACK})
+  return {
+    type: NAVIGATE_BACK
   }
 }
 
-export function goAndReplace(route) {
-  return dispatch => {
-    dispatch({type: NAVIGATE_REPLACE, route})
-    nav.replace(route)
-    dispatch({type: UPDATE_HISTORY})
+export function goAndReplace(key, state) {
+  return {
+    type: NAVIGATE_REPLACE,
+    key,
+    state
   }
 }
 
-export function resetTo(route) {
-  return dispatch => {
-    dispatch({type: NAVIGATE_RESET, route})
-    nav.resetTo(route)
-    dispatch({type: UPDATE_HISTORY})
+export function resetTo(state) {
+  return {
+    type: NAVIGATE_RESET,
+    state
   }
 }
 
 const initialState = {
-  init: {name: 'launch'},
-  current: {},
-  prevision: {},
-  history: []
+  key: 'Root',
+  index: 0,
+  children: [
+		{ key: 'home' }
+  ]
 }
 
 export default function navigation(state = initialState, action) {
   switch (action.type) {
   case NAVIGATE_TO:
-    return {...state,
-      current: action.route,
-      prevision: state.current,
-      history: nav.getCurrentRoutes()
-    }
+    return NavigationStateUtils.push(state, action.state)
 
-  case NAVIGATE_BACK: {
-    const {history} = state
-    return {...state,
-      current: state.prevision,
-      prevision: history.length >= 2 ? history[history.length - 2] : {},
-      history: nav.getCurrentRoutes()
+  case NAVIGATE_BACK:
+    if (state.index === 0 || state.children.length === 1) {
+      return state
     }
-  }
+    return NavigationStateUtils.pop(state)
 
   case NAVIGATE_RESET:
-    return {...state,
-      current: action.route,
-      prevision: {},
-      history: nav.getCurrentRoutes()
-    }
+    return NavigationStateUtils.reset(state, action.state)
 
   case NAVIGATE_REPLACE:
-    return {...state,
-      current: action.route,
-      history: nav.getCurrentRoutes()
-    }
+    return NavigationStateUtils.replaceAt(state, action.key, action.state)
 
-  case UPDATE_HISTORY:
-    return {...state,
-      history: nav.getCurrentRoutes()
-    }
   default:
     return state
   }
