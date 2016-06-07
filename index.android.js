facc1520ea93a19e03f5bb016335ddce46b38dac
Terry Sahaidak <s_tasssik_taras@ukr.net>
2016-06-07 19:44:11 +0300

    1.0.0-alpha-3

--- index.android.js
+++ index.android.js
@@ -1,5 +1,6 @@
-import React, {AppRegistry} from 'react-native'
+import {AppRegistry} from 'react-native'
 import GitterMobile from './app'
-global.CURRENT_VERSION = '1.0.0-alpha-2'
+
+global.CURRENT_VERSION = '1.0.0-alpha-3'
 
 AppRegistry.registerComponent('gittermobile', () => GitterMobile);
