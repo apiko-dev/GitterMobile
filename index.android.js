d2e787560ea8b3b248bdf161b3eb885fa9f1b36b
Terry Sahaidak <s_tasssik_taras@ukr.net>
2016-06-11 23:49:25 +0300

    Bump version number 0.4.0

--- index.android.js
+++ index.android.js
@@ -1,6 +1,6 @@
 import {AppRegistry} from 'react-native'
 import GitterMobile from './app'
 
-global.CURRENT_VERSION = '1.0.0-alpha-3'
+global.CURRENT_VERSION = 'v0.4.0'
 
 AppRegistry.registerComponent('gittermobile', () => GitterMobile);
