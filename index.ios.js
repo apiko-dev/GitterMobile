371d5626bc6bd63935ac8b461771484aa7e904fe
Terry Sahaidak <s_tasssik_taras@ukr.net>
2016-10-12 13:57:40 +0300

    Fix faye

--- index.ios.js
+++ index.ios.js
@@ -2,4 +2,6 @@ import React from 'react';
 import {AppRegistry} from 'react-native';
 import GitterMobile from './app'
 
+global.CURRENT_VERSION = 'v0.5.2'
+
 AppRegistry.registerComponent('gittermobile', () => GitterMobile);
