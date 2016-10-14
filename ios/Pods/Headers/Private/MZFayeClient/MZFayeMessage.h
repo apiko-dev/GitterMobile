60b01b7d5b8a80bf6a2b4b8611f3c6a300f13a07
Terry Sahaidak <s_tasssik_taras@ukr.net>
2016-10-14 14:47:59 +0300

    Add cocoapods support

---
+++ ios/Pods/Headers/Private/MZFayeClient/MZFayeMessage.h
@@ -0,0 +1,1 @@
+../../../MZFayeClient/MZFayeClient/MZFayeMessage.h
---
+++ ios/Pods/Target Support Files/MZFayeClient/MZFayeClient-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MZFayeClient : NSObject
+@end
+@implementation PodsDummy_MZFayeClient
+@end
