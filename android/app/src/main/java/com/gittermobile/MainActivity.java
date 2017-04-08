782e418fe65dd1159e13e7769bf43c1f9bb379dc
Terry Sahaidak <s_tasssik_taras@ukr.net>
2017-04-08 17:29:13 +0200

    Upgrade to latest RN

--- android/app/src/main/java/com/gittermobile/MainActivity.java
+++ android/app/src/main/java/com/gittermobile/MainActivity.java
@@ -2,16 +2,14 @@ package com.gittermobile;
 
 import com.facebook.react.ReactActivity;
 
-import android.content.Intent;
-import android.content.res.Configuration;
-
 public class MainActivity extends ReactActivity {
+
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
-     @Override
-     protected String getMainComponentName() {
-         return "gittermobile";
-     }
+    @Override
+    protected String getMainComponentName() {
+        return "gittermobile";
+    }
 }
