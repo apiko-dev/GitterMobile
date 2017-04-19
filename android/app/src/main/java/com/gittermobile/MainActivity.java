e49f5f1c5aa315756f5da3e565eb26f2490fb4c1
Terry Sahaidak <s_tasssik_taras@ukr.net>
2017-04-19 23:09:39 +0200

    Add custom android splash screen

--- android/app/src/main/java/com/gittermobile/MainActivity.java
+++ android/app/src/main/java/com/gittermobile/MainActivity.java
@@ -1,9 +1,31 @@
 package com.gittermobile;
 
 // import com.facebook.react.ReactActivity;
+import android.widget.LinearLayout;
+import android.graphics.Color;
+import android.widget.TextView;
+import android.view.Gravity;
+import android.util.TypedValue;
 import com.reactnativenavigation.controllers.SplashActivity;
 
 public class MainActivity extends SplashActivity {
+
+    @Override
+    public LinearLayout createSplashLayout() {
+        LinearLayout view = new LinearLayout(this);
+        TextView textView = new TextView(this);
+
+        view.setBackgroundColor(Color.parseColor("#E20354"));
+        view.setGravity(Gravity.CENTER);
+
+        textView.setTextColor(Color.parseColor("#FFFFFF"));
+        textView.setText("GitterMobile");
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 40);
+
+        view.addView(textView);
+        return view;
+    }
 
     // /**
     //  * Returns the name of the main component registered from JavaScript.
