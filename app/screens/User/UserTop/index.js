3c5b83d4bf809cea80c2368cf15241e61537b2b0
AndreyKogut <andreyko.ol@gmail.com>
2017-05-29 10:35:20 +0300

    Twitter users fix

--- app/screens/User/UserTop/index.js
+++ app/screens/User/UserTop/index.js
@@ -4,39 +4,37 @@ import Avatar from '../../../components/Avatar'
 import s from './styles'
 import {createGhAvatarLink} from '../../../utils/links'
 
-const UserTop = ({displayName, username, github}) => {
-  return (
-    <View style={s.container}>
-      <View style={s.avatarWrapper}>
-        <Avatar
-          src={createGhAvatarLink(username, 200)}
-          size={80} />
-      </View>
-      <View style={s.displayNameWapper}>
-        <Text style={s.displayName}>
-          {displayName}
-        </Text>
-        <Text style={s.username}>
-          @{username}
-        </Text>
-      </View>
-      <View style={s.github}>
-        <View style={s.githubItem}>
-          <Text style={s.bold}>{github.followers}</Text>
-          <Text style={s.githubItemText}>followers</Text>
-        </View>
-        <View style={s.githubItem}>
-          <Text style={s.bold}>{github.public_repos}</Text>
-          <Text style={s.githubItemText}>repos</Text>
-        </View>
-        <View style={s.githubItem}>
-          <Text style={s.bold}>{github.following}</Text>
-          <Text style={s.githubItemText}>following</Text>
-        </View>
-      </View>
+const UserTop = ({displayName, username, github}) => (
+  <View style={s.container}>
+    <View style={s.avatarWrapper}>
+      <Avatar
+        src={createGhAvatarLink(username, 200)}
+        size={80} />
     </View>
-  )
-}
+    <View style={s.displayNameWapper}>
+      <Text style={s.displayName}>
+        {displayName}
+      </Text>
+      <Text style={s.username}>
+        @{username}
+      </Text>
+    </View>
+    {github && <View style={s.github}>
+      <View style={s.githubItem}>
+        <Text style={s.bold}>{github.followers}</Text>
+        <Text style={s.githubItemText}>followers</Text>
+      </View>
+      <View style={s.githubItem}>
+        <Text style={s.bold}>{github.public_repos}</Text>
+        <Text style={s.githubItemText}>repos</Text>
+      </View>
+      <View style={s.githubItem}>
+        <Text style={s.bold}>{github.following}</Text>
+        <Text style={s.githubItemText}>following</Text>
+      </View>
+    </View>}
+  </View>
+)
 
 UserTop.propTypes = {
   displayName: PropTypes.string,
