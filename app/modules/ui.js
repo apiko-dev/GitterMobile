396d330643dcec3afd85f87c0484030891091db6
Terry Sahaidak <s_tasssik_taras@ukr.net>
2017-06-30 02:28:30 +0300

    Save input draft per room into async storage

--- app/modules/ui.js
+++ app/modules/ui.js
@@ -3,27 +3,29 @@ import {getItem, setItem, removeItem} from '../utils/storage'
 export const INITIALIZE_UI = 'ui/INITIALIZE_UI'
 export const CHANGE_ROOM_INFO_DRAWER_STATE = 'ui/RoomInfo/CHANGE_ROOM_INFO_DRAWER_STATE'
 export const TOGGLE_DRAWER_SECTION_STATE = 'ui/TOGGLE_DRAWER_SECTION_STATE'
+export const SET_ROOM_INPUT_STATE = 'ui/SET_ROOM_INPUT_STATE'
 
 export function initializeUi() {
-  return async (dispatch, getState) => {
-    const sectionsStateFromStore = await getItem('sectionsState')
-    if (!sectionsStateFromStore) {
-      const {sectionsState} = getState().ui
-      await setItem('sectionsState', JSON.stringify(sectionsState))
-    } else {
-      dispatch({type: INITIALIZE_UI, payload: {
-        sectionsState: JSON.parse(sectionsStateFromStore)
-      }})
-    }
+  return async dispatch => {
+    const payload = await getItem('uiReducer')
+
+    dispatch({type: INITIALIZE_UI, payload: JSON.parse(payload)})
   }
 }
 
 export function toggleDrawerSectionState(sectionName, oldState) {
-  return async (dispatch, getState) => {
+  return async dispatch => {
     dispatch({type: TOGGLE_DRAWER_SECTION_STATE, sectionName, newState: !oldState})
 
-    const {sectionsState} = getState().ui
-    await setItem('sectionsState', JSON.stringify(sectionsState))
+    await dispatch(saveReducerToStorage())
+  }
+}
+
+export function setRoomTextInputState(roomId, text) {
+  return async dispatch => {
+    dispatch({type: SET_ROOM_INPUT_STATE, roomId, text})
+
+    await dispatch(saveReducerToStorage())
   }
 }
 
@@ -34,6 +36,12 @@ export function changeRoomInfoDrawerState(state) {
   }
 }
 
+function saveReducerToStorage() {
+  return async (dispatch, getState) => {
+    await setItem('uiReducer', JSON.stringify(getState().ui))
+  }
+}
+
 const initialState = {
   roomInfoDrawerState: 'close',
   sectionsState: {
@@ -41,14 +49,15 @@ const initialState = {
     Unread: false,
     Channels: false,
     Organizations: false
-  }
+  },
+  roomInputStateById: {}
 }
 
 export default function ui(state = initialState, action) {
   switch (action.type) {
 
   case INITIALIZE_UI:
-    return Object.assign({}, state, action.payload)
+    return Object.assign({}, state, action.payload ? action.payload : initialState)
 
   case CHANGE_ROOM_INFO_DRAWER_STATE:
     return {...state,
@@ -59,6 +68,13 @@ export default function ui(state = initialState, action) {
     return {...state,
       sectionsState: {...state.sectionsState,
         [action.sectionName]: action.newState
+      }
+    }
+
+  case SET_ROOM_INPUT_STATE:
+    return {...state,
+      roomInputStateById: {...state.roomInputStateById,
+        [action.roomId]: action.text
       }
     }
 
